38060de5eb26e668a87acc9815132e91d449f357
erenavar <eren.avar@hotmail.com>
2023-09-25 20:57:31 +0300

    Added New Properties
    Added BookType class,entity framework and connected to Db

---
+++ BookApplication/Models/BookType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookApplication.Models
+{
+    public class BookType
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+    }
+}
