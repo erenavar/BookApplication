a7c4f4db2fa22ea07227334f606fd1ac10b71cfb
erenavar <eren.avar@hotmail.com>
2023-09-26 00:51:26 +0300

    Added first table.

---
+++ BookApplication/Utility/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using BookApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookApplication.Utility
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<BookType> BookTypes { get; set; }
+    }
+}
