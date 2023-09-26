dec213a75af106dac664ff977a9fde4d038ba475
erenavar <eren.avar@hotmail.com>
2023-09-26 23:51:36 +0300

    Connected DB and pull book types from DB.

--- BookApplication/Controllers/BookTypeController.cs
+++ BookApplication/Controllers/BookTypeController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookApplication.Models;
+using BookApplication.Utility;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookApplication.Controllers
 {
     public class BookTypeController : Controller
     {
+
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public BookTypeController(ApplicationDbContext context)
+        {
+            _applicationDbContext = context;
+        }
         public IActionResult Index()
         {
+            List<BookType> objBookTypeList = _applicationDbContext.BookTypes.ToList();
             return View();
         }
     }
