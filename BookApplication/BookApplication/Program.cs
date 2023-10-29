e8d06c115f4f93b6a6a8d1f61772908dcdeb6ba0
Eren AVAR <eren.avar@hotmail.com>
2023-10-29 20:56:35 +0300

    Added IEmailSender
    Error was blocked.

--- BookApplication/BookApplication/Program.cs
+++ BookApplication/BookApplication/Program.cs
@@ -2,6 +2,7 @@ using BookApplication.Models;
 using BookApplication.Utility;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+                                      .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.AddRazorPages();
 
 //Created _bookTypeRepository
@@ -20,8 +22,13 @@ builder.Services.AddScoped<IBookTypeRepository, BookTypeRepository>();
 //Created _bookTypeRepository
 builder.Services.AddScoped<IRentRepository, RentRepository>();
 
-//// Created _bookRepository
+// Created _bookRepository
 builder.Services.AddScoped<IBookRepository, BookRepository>();
+
+//Added for Email Error
+builder.Services.AddScoped<IEmailSender, EmailSender>();
+
+
 
 
 var app = builder.Build();
---
+++ BookApplication/BookApplication/Utility/EmailSender.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace BookApplication.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            // Added for Email Error
+            return Task.CompletedTask;
+        }
+    }
+}
